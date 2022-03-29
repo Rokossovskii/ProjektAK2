fe30cdfc572dcf9979603fc32570301e747dcb52
Michal13241 <84573248+Michal13241@users.noreply.github.com>
2022-03-29 06:11:39 +0200

    fix

--- AK2/arithmeticLibrary.cpp
+++ AK2/arithmeticLibrary.cpp
@@ -1,21 +1,21 @@
 #include "arithmeticLibrary.h"
 
-bool* addInU2(bool* num1, int length1, int przecinek1, bool* num2, int length2, int przecinek2);
+bool* addInU2(bool* num1, int length1, int przecinek1, bool* num2, int length2, int przecinek2)
 {
 	return nullptr;
 }
 
-bool* subInU2(bool* num1, int length1, int przecinek1, bool* num2, int length2, int przecinek2);
+bool* subInU2(bool* num1, int length1, int przecinek1, bool* num2, int length2, int przecinek2)
 {
 	return nullptr;
 }
 
-bool* mulInU2(bool* num1, int length1, int przecinek1, bool* num2, int length2, int przecinek2);
+bool* mulInU2(bool* num1, int length1, int przecinek1, bool* num2, int length2, int przecinek2)
 {
 	return nullptr;
 }
 
-bool* divInU2(bool* num1, int length1, int przecinek1, bool* num2, int length2, int przecinek2);
+bool* divInU2(bool* num1, int length1, int przecinek1, bool* num2, int length2, int przecinek2)
 {
 	return nullptr;
 }
