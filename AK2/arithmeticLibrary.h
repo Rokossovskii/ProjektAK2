7c52d5ad9fb64372da7b0ca800a470faffd15346
Michal13241 <84573248+Michal13241@users.noreply.github.com>
2022-03-29 06:10:12 +0200

    commit

--- AK2/arithmeticLibrary.h
+++ AK2/arithmeticLibrary.h
@@ -1,10 +1,15 @@
 #pragma once
 
+struct num{
+    bool *tab;
+    int length = 0;
+    int przecinek = 0;
+};
 
-bool* addInU2(bool* num1, bool* num2);
+bool* addInU2(bool* num1, int length1, int przecinek1, bool* num2, int length2, int przecinek2);
 
-bool* subInU2(bool* num1, bool* num2);
+bool* subInU2(bool* num1, int length1, int przecinek1, bool* num2, int length2, int przecinek2);
 
-bool* mulInU2(bool* num1, bool* num2);
+bool* mulInU2(bool* num1, int length1, int przecinek1, bool* num2, int length2, int przecinek2);
 
-bool* divInU2(bool* num1, bool* num2);
+bool* divInU2(bool* num1, int length1, int przecinek1, bool* num2, int length2, int przecinek2);
